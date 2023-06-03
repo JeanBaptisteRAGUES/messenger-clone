9dbcc0071575b22f8a466056a0ef50985d43922a
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2023-06-04 01:48:31 +0200

    Ajout de la possibilité de se connecter avec son compte Github ou Google, cependant il y a une erreur qui apparait.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -9,6 +9,8 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import AuthSocialButton from "./AuthSocialButton";
 import axios from "axios";
+import {toast} from "react-hot-toast";
+import {signIn} from "next-auth/react";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -43,17 +45,42 @@ const AuthForm = () => {
 
         if(variant === 'REGISTER'){
             axios.post('/api/register', data)
+            .catch(() => toast.error('Something went wrong !'))
+            .finally(() => setIsLoading(false));
         }
 
         if(variant === 'LOGIN'){
-            // NextAuth SignIn
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+            .then((callback) => {
+                if(callback?.error) {
+                    toast.error('Invalid credentials');
+                }
+
+                if(callback?.ok && !callback?.error) {
+                    toast.success('Logged in !');
+                }
+            })
+            .finally(() => setIsLoading(false));
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true);
 
-        // NextAuth Social Sign in
+        signIn(action, { redirect: false })
+        .then((callback) => {
+            if(callback?.error) {
+                toast.error('Invalid credentials');
+            }
+
+            if(callback?.ok && !callback?.error) {
+                toast.success('Logged in !');
+            }
+        })
+        .finally(() => setIsLoading(false));
     }
 
     return (
