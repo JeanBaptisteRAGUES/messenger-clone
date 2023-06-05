222655005e2f3fb3878401c1df22e626645c1f70
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2023-06-05 20:41:57 +0200

    Récupération des données de l'utilisateur lors de la connexion, ajout de l'avatar.

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,24 @@
 'use client'
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
+import { User } from "@prisma/client";
+
+import useRoutes from "@/app/hooks/useRoutes";
+
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({currentUser});
 
     return (
         <div className=" hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto
@@ -24,6 +36,11 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+            </nav>
+            <nav className=" mt-4 flex flex-col justify-between items-center" >
+                        <div onClick={() => setIsOpen(true)} className=" cursor-pointer hover:opacity-75 transition" >
+                            <Avatar user={currentUser} />
+                        </div>
             </nav>
         </div>
     )
